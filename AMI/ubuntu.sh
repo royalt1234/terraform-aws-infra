19a2124e9edaff3753bb90f6fabe72d999123cdb
royalt1234 <thursdayfrip@gmail.com>
2022-06-13 15:15:05 +0100

    changes

---
+++ AMI/ubuntu.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+sudo add-apt-repository universe
+
+sudo apt update
+
+sudo apt install -y default-jre
+
+sudo apt install -y default-jdk
+
+sudo apt install -y  git mysql-client wget vim telnet htop python3 chrony net-tools
